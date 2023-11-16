2321e987e4f0bda477b32e5bbce9590bef5cd878
Yetity <hlonipoole692@gmail.com>
2023-11-16 06:32:03 +0200

    working tests

--- src/index.test.js
+++ src/index.test.js
@@ -15,50 +15,63 @@ describe("Page Creation", function () {
     });
     describe("Pager#addPage", function () {
         a.addPage("con");
-        expect(a.pages[0].content).to.be.eql("con");
-        expect(a.pages[0].title).to.be(undefined);
-        a.removePage(0);
-        a.addPage({ title: "me", content: "con" });
-        expect(a.pages[0].content).to.be("con");
-        expect(a.pages[0].title).to.be("me");
-        a.removePage(0);
+	describe("add page from string", function (){
+		it("should add a page from a string", function () {
+        		expect(a.pages[0].content).to.be("con");
+        		expect(a.pages[0].title).to.be(undefined);
+		})
+	})
+	describe("add page from object", function () {
+		it("should add a page from an object", function () {
+        		a.addPage({ title: "me", content: "con" });
+        		expect(a.pages[1].content).to.be("con");
+        		expect(a.pages[1].title).to.be("me");
+		})
+	})
     });
     describe("Pager#addPages", function () {
-        it("should add 2 pages from a string.", function () {
-            a.addPages("con", "con2");
-            expect(a.pages.length).to.be(2);
-            expect(a.pages[0].content).to.be("con");
-            expect(a.pages[0].title).to.be(undefined);
-            expect(a.pages[1].content).to.be("con2");
-            expect(a.pages[1].title).to.be(undefined);
+	let str = new Pager()
+        describe("should add 2 pages from a string.", function () {
+            str.addPages("con", "con2");
+	    it("should be of length 2", function () {
+            expect(str.pages).to.have.length(2);
+	    })
+	    it("should have the pages", function () {
+            expect(str.pages[0].content).to.be("con");
+            expect(str.pages[0].title).to.be(undefined);
+            expect(str.pages[1].content).to.be("con2");
+            expect(str.pages[1].title).to.be(undefined);
+	    })
         });
-        a.pages.splice(0, 2);
-        it("should add 3 pages from an object", function () {
-            a.addPages(
+	let strr = new Pager()
+        describe("should add 3 pages from an object", function () {
+            strr.addPages(
                 { title: "me", content: "con" },
                 { title: "me again", content: "con2" },
                 { title: "yup its me", content: "mhm" }
             );
             it("should hold 3 objects", function () {
-                expect(a.pages.length).to.be(3);
+                expect(strr.pages).have.length(3);
             });
-            expect(a.pages[0].content).to.be("con");
-            expect(a.pages[0].title).to.be("me");
+	    it("should have the pages", function () {
+            expect(strr.pages[0].content).to.be("con");
+            expect(strr.pages[0].title).to.be("me");
 
-            expect(a.pages[1].content).to.be("con2");
-            expect(a.pages[1].title).to.be("me again");
+            expect(strr.pages[1].content).to.be("con2");
+            expect(strr.pages[1].title).to.be("me again");
 
-            expect(a.pages[2].content).to.be("mhm");
-            expect(a.pages[2].title).to.be("yup its me");
+            expect(strr.pages[2].content).to.be("mhm");
+            expect(strr.pages[2].title).to.be("yup its me");
+	    })
         });
     });
     describe("Pager#removePage", function () {
-        // We didnt remove the pages from the previous test, therefore there is currently 3 pages
-        a.addPages("removed", "random stuff", "mh");
-        // Page 4's content = "removed" so we check for that.
+	let rem = new Pager()
+        
+	rem.addPages("removed", "random stuff", "mh");
         it("should remove page index 3 (page 4)", function () {
-            a.removePage(3);
-            expect(a.pages[3].content).to.not.be("removed");
+            rem.removePage(0);
+            expect(rem.pages[0].content).to.not.be("removed");
         });
     });
     describe("Pager#addDynamicPages", function () {
